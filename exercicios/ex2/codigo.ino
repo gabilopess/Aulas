0409252fd091a5e53dec2ca8233911ca22bbc183
gabilopess <99339161+gabilopess@users.noreply.github.com>
2022-03-09 11:28:36 -0300

    Create codigo.ino

---
+++ exercicios/ex2/codigo.ino
@@ -0,0 +1,18 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  
+}
+
+void loop()
+{
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  delay(1000); 
+  digitalWrite(4, HIGH);
+  digitalWrite(3, LOW);
+  delay(1000); 
+}
