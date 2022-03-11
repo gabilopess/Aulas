ff2a1ac58b5cd99a87ef740cdaad94ba37ec3742
gabilopess <99339161+gabilopess@users.noreply.github.com>
2022-03-11 11:16:36 -0300

    Create codigo.ino

---
+++ exercicios/ex3/codigo.ino
@@ -0,0 +1,31 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT);
+  
+}
+
+void loop()
+{
+  digitalWrite(6, HIGH);
+  delay(2000);
+  digitalWrite(5, HIGH);
+  delay(2000); 
+  digitalWrite(4, HIGH);
+  delay(2000);
+  digitalWrite(3, HIGH);
+  delay(2000);
+  digitalWrite(2, HIGH);
+  delay(2000);
+  digitalWrite(6, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
+  delay(2000);
+}
